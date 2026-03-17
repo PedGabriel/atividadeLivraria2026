0cc3dc200635e1dc2731da0cc6f96c78086f636f
PedGabriel <pedrogabrielgoncalves91@gmail.com>
2026-03-17 16:30:40 -0300

    FEAT: Desenvolvimento da relação entre as tabelas autor e livro

--- core/models/livro.py
+++ core/models/livro.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+from .autor import Autor
 from .categoria import Categoria
 from .editora import Editora
 
@@ -23,6 +24,11 @@ class Livro(models.Model):
         null=True,
         blank=True,
     )
+    autores = models.ManyToManyField(
+        Autor,
+        related_name='livros',
+        blank=True,
+        )
 
     def __str__(self):
-        return f'{self.titulo} - {self.categoria}'
+        return f'{self.titulo} - {self.categoria} - {self.quantidade}'
