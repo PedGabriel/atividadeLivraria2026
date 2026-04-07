07ef3ff134b0d5783b754c1a377a22b9e27eff5d
PedGabriel <pedrogabrielgoncalves91@gmail.com>
2026-04-07 16:35:36 -0300

    FEAT: APP de upload

--- app/urls.py
+++ app/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from drf_spectacular.views import (
@@ -13,6 +15,7 @@ from rest_framework_simplejwt.views import (
 )
 
 from core.views import AutorViewSet, CategoriaViewSet, EditoraViewSet, LivroViewSet, UserRegistrationView, UserViewSet
+from uploader.router import router as uploader_router
 
 router = DefaultRouter()
 
@@ -25,6 +28,7 @@ router.register(r'livros', LivroViewSet, basename='livros')
 urlpatterns = [
     path('admin/', admin.site.urls),
     # OpenAPI 3
+    path('api/media/', include(uploader_router.urls)),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path(
         'api/doc/',
@@ -45,3 +49,5 @@ urlpatterns = [
     # API
     path('api/', include(router.urls)),
 ]
+
+urlpatterns += static(settings.MEDIA_ENDPOINT, document_root=settings.MEDIA_ROOT)
--- core/serializers/livro.py
+++ core/serializers/livro.py
@@ -1,15 +1,19 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, SlugRelatedField
+
+from uploader.models import Image
+from uploader.serializers import ImageSerializer
 
 from core.models import Livro
 
 
-class LivroRetriveSerializer (ModelSerializer):
+class LivroListSerializer (ModelSerializer):
     class Meta:
         fields = ('titulo', 'preco', 'id')
         model = Livro
 
 
-class LivroListSerializer (ModelSerializer):
+class LivroRetriveSerializer (ModelSerializer):
+    capa = ImageSerializer(required=False)
     class Meta:
         fields = '__all__'
         model = Livro
@@ -17,6 +21,14 @@ class LivroListSerializer (ModelSerializer):
 
 
 class LivroSerializer (ModelSerializer):
+    capa_attachment_key = SlugRelatedField(
+        source='capa',
+        queryset=Image.objects.all(),
+        slug_field='attachment_key',
+        required=False,
+        write_only=True,
+    )
+    capa = ImageSerializer(required=False, read_only=True)
     class Meta:
         fields = '__all__'
         model = Livro
