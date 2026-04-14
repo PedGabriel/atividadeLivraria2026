a79aac9abfe77b2644c390e774abe713d8b15411
PedGabriel <pedrogabrielgoncalves91@gmail.com>
2026-04-14 16:03:08 -0300

    FEAT: dump e load e dados

--- core/models/editora.py
+++ core/models/editora.py
@@ -4,6 +4,8 @@ from django.db import models
 class Editora(models.Model):
     site = models.URLField(blank=True, null=True, max_length=100)
     nome = models.CharField(max_length=200)
+    email = models.EmailField(blank=True, null=True, max_length=100)
+    cidade = models.CharField(blank=True, null=True, max_length=100)
 
     def __str__(self):
         return f"{self.nome} - {self.site}"
